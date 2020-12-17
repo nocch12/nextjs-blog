0d06ddfa12bdb4c794d34c59940f1e944a8e065e
Kohki Ohno <k.ohno12.gt@gmail.com>
2020-12-17 18:26:55 +0900

    siteTitle変更

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Kohki";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Kohki's Blog";
 
 export default function Layout({ children, home }) {
   return (
