8aea0923503010c6220898d67c06b4368ff9df6e
Kohki Ohno <k.ohno12.gt@gmail.com>
2022-03-10 00:07:08 +0900

    ChakraLink エラー修正

--- components/ChakraLink.tsx
+++ components/ChakraLink.tsx
@@ -1,14 +1,20 @@
 import NextLink from 'next/link';
 import { VFC, PropsWithChildren } from 'react';
 import { Link, LinkProps } from '@chakra-ui/react';
+import { UrlObject } from 'url';
 
-const ChakraLink: VFC<PropsWithChildren<LinkProps>> = ({
+type Props = LinkProps |  {
+  href: UrlObject;
+}
+
+
+const ChakraLink: VFC<PropsWithChildren<Props>> = ({
   href,
   children,
   ...props
 }) => {
   return (
-    <NextLink href={href} passHref>
+    <NextLink href={href || ''} passHref>
       <Link {...props}>{children}</Link>
     </NextLink>
   );
